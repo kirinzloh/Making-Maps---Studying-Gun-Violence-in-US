e7e309da361611cc6c5602945a80bd30e7064b2a
Wei Quan <weiquan7@hotmail.com>
2018-04-28 19:33:23 +0800

    added index page

--- STOPGunViolence/mapsproject/static/js/custom.js
+++ STOPGunViolence/mapsproject/static/js/custom.js
@@ -79,7 +79,7 @@ $(function(){
 			$('#'+$(this).val()).fadeIn();
 		}
 	});
-	
+
 	$('#search_filter').change(function() {
 		var url = new URL(window.location.href);
 		var q = url.searchParams.get("q");
@@ -88,10 +88,10 @@ $(function(){
 		name = $('#checkbox_name').is(":checked")
 		creator = $('#checkbox_creator').is(":checked")
 		genre = $('#checkbox_genre').is(":checked")
-		
+
 		window.location = "/search/?q=" + q + "&name=" + name + "&username=" + creator + "&genre=" + genre + "&sort=" + sort
 	});
-	
+
 	$('#search_sorting').change(function() {
 		var url = new URL(window.location.href);
 		var q = url.searchParams.get("q");
@@ -103,14 +103,14 @@ $(function(){
 		console.log(sort);
 		window.location = "/search/?q=" + q + "&name=" + name + "&username=" + creator + "&genre=" + genre + "&sort=" + sort
 	});
-	
+
 	$('#stats_filter').change(function() {
 		var url = new URL(window.location.href);
 		var sort = url.searchParams.get("sort");
 		var filters = $('#stats_filter option:selected').val()
 		window.location = window.location.pathname + "?filters=" + filters + "&sort=" + sort
 	});
-	
+
 	$('#stats_sorting').change(function() {
 		var url = new URL(window.location.href);
 		var filters = url.searchParams.get("filters");
@@ -131,7 +131,7 @@ function follow(id, origin) {
 
 function unfollow(id, origin) {
 	event.stopPropagation();
-	window.location = "/unfollow/" + id + "/" + origin 
+	window.location = "/unfollow/" + id + "/" + origin
 }
 
 function comment_rate(id, up, dataset) {
